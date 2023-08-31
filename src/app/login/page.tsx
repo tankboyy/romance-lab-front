9b319521c13bd4ce79b84dd5e0bdf6d29eaeabac
돌중 <doljung@doljung-ui-MacBookAir.local>
2023-08-31 20:40:02 +0900

    💄 로그인 페이지 수정 피그마 로그인페이지를 참고하여 로그인페이지 디자인완성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 import React from 'react';
 import Image from 'next/image'
-import loginImage from '../../../public/loginImage.png'
+import loginImage from '../../../public/login/icon.png'
+import kakaoBtn from '../../../public/login/kakao.png'
+import googleIcon from '../../../public/login/google.png'
 
 type propsType = {
 
@@ -11,20 +13,20 @@ export default function Home(props: propsType) {
   return (
     <div className="w-[390px] h-[667px]">
       <div className="text-center flex flex-col items-center text-[20px] my-[70px]">
-        <p>우리의 이야기로 배부르게</p>
-        <p>우리의 감정으로 행복하게</p>
-        <Image className="my-[18px]" alt="양이에오" src={loginImage}/>
-        <p className="text-[24px]">애정염소</p>
+        <Image className="mb-[31px]" alt="양이에오" src={loginImage}/>
+        <p className="text-[24px] font-bold">애정연구소</p>
+        <p className="text-[16px]">더 깊고 풍부한 애정을 연구하다</p>
       </div>
       <div className="flex flex-col justify-center items-center">
         <div>
-          <button className="w-[330px] h-[50px] rounded-[5px] border-[1px] border-[solid] border-[#E9E9E9] bg-[#FFF] shadow-login mb-[20px]">
-            <p className="text-[18px]">구글로 계속하기</p>
+          <button className="flex justify-center items-center w-[330px] h-[44px] rounded-[5px] border-[1px] border-[solid] border-[#E9E9E9] bg-[#FFF] shadow-login mb-[14px]">
+            <Image src={googleIcon} alt="구글"/>
+            <p className="ml-[6px] text-[18px]">구글로 계속하기</p>
           </button>
         </div>
         <div>
-          <button className="w-[330px] h-[50px] rounded-[5px] bg-[#FFF500] shadow-login mb-[30px]">
-            <p className="text-[18px]">카카오로 계속하기</p>
+          <button>
+            <Image alt="카카오" src={kakaoBtn}/>
           </button>
         </div>
         <div>
--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,1 +1,27 @@
-``
+"use client"
+
+import React, {useState} from 'react';
+
+type propsType = {
+  
+};
+
+export default function Home(props: propsType) {
+  const [step, setStep] = useState<"회원가입" | "로그인">("회원가입")
+  return (
+    <div>
+      <div className="flex items-center justify-center text-[18px]">
+        <p className={`${step === '회원가입' ? "mx-[10px] text-[#000] underline underline-offset-1" : "mx-[10px] text-[#AAA]"}`}>회원가입</p>
+        <p className={`${step === '로그인' ? "mx-[10px] text-[#000] underline underline-offset-1" : "mx-[10px] text-[#AAA]"}`}>로그인</p>
+      </div>
+      <div
+        className="justify-center items-center text-center"
+      >
+        <div className="flex flex-col">
+          <p className="text-[14px] text-left">이메일</p>
+          <input type="email"/>
+        </div>
+      </div>
+    </div>
+  );
+}
