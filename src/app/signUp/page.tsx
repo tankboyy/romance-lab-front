372048c9295899bdfeab344cb9a458cfb204bac5
돌중 <doljung@doljung-ui-MacBookAir.local>
2023-09-01 23:07:30 +0900

    💄 회원가입 페이지 수정
    회원가입 페이지 인풋 컴포넌트 작성중

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,27 +1,21 @@
 "use client"
 
 import React, {useState} from 'react';
+import InputComponent from "./InputComponent";
 
-type propsType = {
-  
-};
+type propsType = {};
 
 export default function Home(props: propsType) {
   const [step, setStep] = useState<"회원가입" | "로그인">("회원가입")
   return (
     <div>
       <div className="flex items-center justify-center text-[18px]">
-        <p className={`${step === '회원가입' ? "mx-[10px] text-[#000] underline underline-offset-1" : "mx-[10px] text-[#AAA]"}`}>회원가입</p>
-        <p className={`${step === '로그인' ? "mx-[10px] text-[#000] underline underline-offset-1" : "mx-[10px] text-[#AAA]"}`}>로그인</p>
+        <p
+          className={`${step === '회원가입' ? "mx-[10px] text-[#000] underline underline-offset-1" : "mx-[10px] text-[#AAA]"}`}>회원가입</p>
+        <p
+          className={`${step === '로그인' ? "mx-[10px] text-[#000] underline underline-offset-1" : "mx-[10px] text-[#AAA]"}`}>로그인</p>
       </div>
-      <div
-        className="justify-center items-center text-center"
-      >
-        <div className="flex flex-col">
-          <p className="text-[14px] text-left">이메일</p>
-          <input type="email"/>
-        </div>
-      </div>
+      <InputComponent title="이메일" failText="유효하지 않은 이메일이에요" successText="몰라"/>
     </div>
   );
 }
